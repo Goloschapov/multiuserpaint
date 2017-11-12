4f8499debdc047f9686e005e24e697b6abc0aa23
Tema <Tema@->
2017-11-13 00:09:26 +0400

    сервер2

--- MultiUserPaint/Program.cs
+++ MultiUserPaint/Program.cs
@@ -66,7 +66,7 @@ namespace MultiUserPaint
             mutexObj.ReleaseMutex();
         }
 
-        static string GetCanvas()
+        static string GetCanvas() 
         {
             string text = "";
             foreach (string pixel in Canvas)
